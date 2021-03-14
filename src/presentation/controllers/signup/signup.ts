840a7c9677be8ee93341926dd235da87d9a7e6e2
Kilderson Sena <dersonsena@gmail.com>
2021-03-14 19:40:20 -0300

    chore: just jest helper script setup

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -32,7 +32,7 @@ export class SignUpController implements Controller {
 
       return ok(account)
     } catch (error) {
-      // console.error(error)
+      console.error(error)
       return serverError()
     }
   }
